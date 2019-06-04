0a7e2b8f1a497c84bcfa1689b80a6c8e8776ad88
Yin <64zhangyin@163.com>
2019-06-04 02:05:52 -0500

    add modal, fix some column bugs

--- mp1/source/scripts/main.js
+++ mp1/source/scripts/main.js
@@ -1,3 +1,4 @@
+//scroll effect
 window.addEventListener('scroll', () => {
     //change the bar height and text fontSize
     let bar = document.getElementById("main-header");
@@ -65,3 +66,67 @@ window.addEventListener('scroll', () => {
             document.getElementById("nav_gallery").style.opacity = 1;
         }
 });
+
+//modal effect
+window.onload = function () {
+    //get modal elem
+    let modal = document.getElementById("gallery-modal");
+    let modalContent1 = document.getElementById("gallery-modal-1");
+    let modalContent2 = document.getElementById("gallery-modal-2");
+    let modalContent3 = document.getElementById("gallery-modal-3");
+    let modalContent4 = document.getElementById("gallery-modal-4");
+    //get open modal btn
+    let modalBtn1 = document.getElementById("modal-link-1");
+    let modalBtn2 = document.getElementById("modal-link-2");
+    let modalBtn3 = document.getElementById("modal-link-3");
+    let modalBtn4 = document.getElementById("modal-link-4");
+    //get close btn
+    let closeBtn1 = document.getElementById("close-btn-1");
+    let closeBtn2 = document.getElementById("close-btn-2");
+    let closeBtn3 = document.getElementById("close-btn-3");
+    let closeBtn4 = document.getElementById("close-btn-4");
+    //listen for click
+    modalBtn1.addEventListener("click", openModal1);
+    modalBtn2.addEventListener("click", openModal2);
+    modalBtn3.addEventListener("click", openModal3);
+    modalBtn4.addEventListener("click", openModal4);
+    //listen for the close action
+    closeBtn1.addEventListener("click", closeModal1);
+    closeBtn2.addEventListener("click", closeModal2);
+    closeBtn3.addEventListener("click", closeModal3);
+    closeBtn4.addEventListener("click", closeModal4);
+
+    function openModal1() {
+        modalContent1.style.display = "block";
+        modal.style.display = "block";
+    }
+    function openModal2() {
+        modalContent2.style.display = "block";
+        modal.style.display = "block";
+    }
+    function openModal3() {
+        modalContent3.style.display = "block";
+        modal.style.display = "block";
+    }
+    function openModal4() {
+        modalContent4.style.display = "block";
+        modal.style.display = "block";
+    }
+    //close function
+    function closeModal1() {
+        modal.style.display = "none";
+        modalContent1.style.display = "none";
+    }
+    function closeModal2() {
+        modal.style.display = "none";
+        modalContent2.style.display = "none";
+    }
+    function closeModal3() {
+        modal.style.display = "none";
+        modalContent3.style.display = "none";
+    }
+    function closeModal4() {
+        modal.style.display = "none";
+        modalContent4.style.display = "none";
+    }
+}
